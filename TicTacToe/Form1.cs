7a0a46d87cb651285efacfeaff0d219e1b6a26b8
Jin Chen <ac.chencho@gmail.com>
2020-02-09 22:12:17 -0500

    Stating conditionals to find the winner.

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -40,7 +40,7 @@ namespace TicTacToe
             button.Text = (xTurn) ? "X" : "O";
             button.ForeColor = (xTurn) ? Color.Blue : Color.Red;
             xTurn = !xTurn;
-            hasWinner = IsWinner();
+            hasWinner = IsWinner(button.Name);
 
             if (hasWinner)
             {
@@ -48,9 +48,67 @@ namespace TicTacToe
             }
         }
 
-        private bool IsWinner()
+        private bool IsWinner(string buttonName)
         {
-            return false;
+            switch (buttonName)
+            {
+                case "A1":
+                    if((A1.Text == A2.Text && A1.Text == A3.Text) || (A1.Text == B2.Text && A1.Text == C2.Text) || (A1.Text == B1.Text && A1.Text == C1.Text))
+                    {
+                        return true;
+                    }
+                    return false;
+                case "A2":
+                    if ((A2.Text == A1.Text && A2.Text == A3.Text) || (A2.Text == B2.Text && A2.Text == C2.Text))
+                    {
+                        return true;
+                    }
+                    return false;
+                case "A3":
+                    if ((A3.Text == A1.Text && A3.Text == A2.Text) || (A3.Text == B3.Text && A3.Text == C3.Text) || (A3.Text == B2.Text && A3.Text == C1.Text))
+                    {
+                        return true;
+                    }
+                    return false;
+                case "B1":
+                    if ((B1.Text == A1.Text && B1.Text == C1.Text) || (B1.Text == B2.Text && B1.Text == B3.Text))
+                    {
+                        return true;
+                    }
+                    return false;
+                case "B2":
+                    if ((B1.Text == B2.Text && B2.Text == B3.Text) || (B2.Text == A2.Text && B2.Text == C2.Text) || (B2.Text == A1.Text && B2.Text == C3.Text) || (B2.Text == A3.Text && B2.Text == C1.Text))
+                    {
+                        return true;
+                    }
+                    return false;
+                case "B3":
+                    if ((B3.Text == B1.Text && B3.Text == B2.Text) || (B3.Text == A3.Text && B3.Text == C3.Text))
+                    {
+                        return true;
+                    }
+                    return false;
+                case "C1":
+                    if ((C1.Text == C2.Text && C1.Text == C3.Text) || (C1.Text == B1.Text && C1.Text == A1.Text) || (C1.Text == B2.Text && C1.Text == A3.Text))
+                    {
+                        return true;
+                    }
+                    return false;
+                case "C2":
+                    if ((C1.Text == C2.Text && C2.Text == C3.Text) || (C2.Text == B2.Text && C2.Text == A2.Text))
+                    {
+                        return true;
+                    }
+                    return false;
+                case "C3":
+                    if ((C3.Text == C2.Text && C3.Text == C1.Text) || (C3.Text == B3.Text && C3.Text == A3.Text) || (C3.Text == B2.Text && C3.Text == A1.Text))
+                    {
+                        return true;
+                    }
+                    return false;
+                default:
+                    return false;
+            }            
         }
 
         private void btn_clear_Click(object sender, EventArgs e)
